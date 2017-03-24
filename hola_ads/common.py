6b1b5311f12cf744d6c987f19552b607705549c8
JhonSmith0x7b <gugoyuoo@gmail.com>
2017-03-24 11:36:12 +0800

    change selector; change s3 address

--- hola_ads/common.py
+++ hola_ads/common.py
@@ -2,7 +2,7 @@
 import hashlib
 
 def get_s3_url(url):
-	return 'http://imgcn.dataverse.cn/%s' % get_s3_uri(url)
+	return 'http://cnimg.dataverse.cn/%s' % get_s3_uri(url)
 	pass
 
 def get_s3_uri(url):
